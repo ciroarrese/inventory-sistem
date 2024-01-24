51b24bbba97c9120459094adde18e5366dd4bc07
Ciro A <ciroarrese.code@gmail.com>
2024-01-24 01:33:46 -0300

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   css/login-style.css
    	modified:   inc/head.php
    	new file:   php/user_lister.php
    	modified:   vistas/user_list.php

--- inc/head.php
+++ inc/head.php
@@ -2,4 +2,5 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Sistema de Inventario</title>
 <link rel="stylesheet" href="./css/bulma.min.css">
-<link rel="stylesheet" href="css/estilos.css">
+<link rel="stylesheet" href="./css/estilos.css">
+<link rel="stylesheet" href="./css/login-style.css">
