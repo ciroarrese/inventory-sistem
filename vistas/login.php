a560f725f7045d5ef1ecf22c686edfc942064d93
Ciro A <ciroarrese.code@gmail.com>
2024-01-26 02:58:07 -0300

    Se cambio el front del login al sistema On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   css/login-style.css
    	new file:   img/pexels-lumn-167698.jpg
    	modified:   vistas/login.php

--- vistas/login.php
+++ vistas/login.php
@@ -1,91 +1,53 @@
-<div class="body-login">
-	<div class="container-login">
-		<div class="login-box">
-			<form action="" method="POST" autocomplete="off">
-				<h2>Login</h2>
-
-				<!-- USER -->
-				<div class="input-box">
-					<input id="user" type="text" name="login_user" pattern="[a-zA-Z0-9$_.\-]{4,20}" maxlength="20" required>
-					<label>User</label>
-				</div>
-
-				<!-- PASS -->
-				<div class="input-box">
-					<input id="pass" type="password" name="login_pass" pattern="[a-zA-Z0-9$@_#&%!¡¿?.\-]{7,100}" maxlength="100" required>
-					<label>Password</label>
-				</div>
-
-				<!-- FORGOT-PASSWORD -->
-				<div class="forgot-password">
-					<a href="#">Forgot Password?</a>
-				</div>
-
-				<!-- BUTTON -->
-				<button type="submit" class="btn-login">Start</button>
-
-				<?php
-
-					if (isset($_POST['login_user']) && isset($_POST['login_pass'])) {
-						require_once './php/main.php';
-						require_once './php/start_session.php';
-					}
-
-				?>
-
-			</form>
+<div class="wrapper">
+	<form class="login-box" action="" method="POST" autocomplete="off">
+		<div class="login-header">
+			<span>Login</span>
 		</div>
 
-		<span style="--i:0;"></span>
-		<span style="--i:1;"></span>
-		<span style="--i:2;"></span>
-		<span style="--i:3;"></span>
-		<span style="--i:4;"></span>
-		<span style="--i:5;"></span>
-		<span style="--i:6;"></span>
-		<span style="--i:7;"></span>
-		<span style="--i:8;"></span>
-		<span style="--i:9;"></span>
-		<span style="--i:10;"></span>
-		<span style="--i:11;"></span>
-		<span style="--i:12;"></span>
-		<span style="--i:13;"></span>
-		<span style="--i:14;"></span>
-		<span style="--i:15;"></span>
-		<span style="--i:16;"></span>
-		<span style="--i:17;"></span>
-		<span style="--i:18;"></span>
-		<span style="--i:19;"></span>
-		<span style="--i:20;"></span>
-		<span style="--i:21;"></span>
-		<span style="--i:22;"></span>
-		<span style="--i:23;"></span>
-		<span style="--i:24;"></span>
-		<span style="--i:25;"></span>
-		<span style="--i:26;"></span>
-		<span style="--i:27;"></span>
-		<span style="--i:28;"></span>
-		<span style="--i:29;"></span>
-		<span style="--i:30;"></span>
-		<span style="--i:31;"></span>
-		<span style="--i:32;"></span>
-		<span style="--i:33;"></span>
-		<span style="--i:34;"></span>
-		<span style="--i:35;"></span>
-		<span style="--i:36;"></span>
-		<span style="--i:37;"></span>
-		<span style="--i:38;"></span>
-		<span style="--i:39;"></span>
-		<span style="--i:40;"></span>
-		<span style="--i:41;"></span>
-		<span style="--i:42;"></span>
-		<span style="--i:43;"></span>
-		<span style="--i:44;"></span>
-		<span style="--i:45;"></span>
-		<span style="--i:46;"></span>
-		<span style="--i:47;"></span>
-		<span style="--i:48;"></span>
-		<span style="--i:49;"></span>
+		<!-- USERNAME -->
+		<div class="input-box">
+			<input class="input-field" id="user" type="text" name="login_user" pattern="[a-zA-Z0-9$_.\-]{4,20}" placeholder=" " maxlength="20" required>
+			<label for="user" class="label">Username</label>
+			<i class="bx bx-user icon"></i>
+		</div>
 
-	</div>
+		<!-- PASS -->
+		<div class="input-box">
+			<input class="input-field" id="pass" type="password" name="login_pass" pattern="[a-zA-Z0-9$@_#&%!¡¿?.\-]{7,100}" placeholder=" " maxlength="100" required>
+			<label for="pass" class="label">Password</label>
+			<i class="bx bx-lock-alt icon"></i>
+		</div>
+
+		<!-- REMEMBER & FORGOT -->
+		<div class="remember-forgot">
+			<div class="remember-me">
+				<input type="checkbox" id="remember">
+				<label for="remember">Remember me</label>
+			</div>
+
+			<div class="forgot">
+				<a href="#">Forgot password?</a>
+			</div>
+		</div>
+
+		<!-- BUTTON -->
+		<div class="input-box">
+			<button type="submit" class="input-submit">Start</button>
+		</div>
+
+		<!-- REGISTER -->
+		<div class="register">
+			<span>Don't have an account? <a href="#">Register</a></span>
+		</div>
+
+		<?php
+
+		if (isset($_POST['login_user']) && isset($_POST['login_pass'])) {
+			require_once './php/main.php';
+			require_once './php/start_session.php';
+		}
+
+		?>
+
+	</form>
 </div>
